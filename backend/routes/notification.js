9852992f20ae90b44f2fd51be8e147a253350fbc
nitendra7 <nitendra8077@gmail.com>
2025-07-25 14:07:43 +0530

    minor changes

--- backend/routes/notification.js
+++ backend/routes/notification.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const notificationController = require('../controllers/notificationController');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/', notificationController.getAllNotifications);
+router.get('/', auth, notificationController.getAllNotifications);
 router.get('/:id', notificationController.getNotificationById);
 router.post('/', notificationController.createNotification);
 router.put('/:id', notificationController.updateNotification);
